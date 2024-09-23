f66f75c442387baf4fa2774d4877c0dafe4027bf
Flamur <flag81@gmail.com>
2024-09-23 14:21:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -240,7 +240,7 @@ app.get("/getProductsByIds", (req, res) => {
   }
   );
 
-app.get("/products", (req, res) => {
+app.get("/products",verifyToken, (req, res) => {
 
 
   let offset1 = parseInt(req.query.offset, 10);
