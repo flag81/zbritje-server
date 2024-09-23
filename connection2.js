6da7fe6ec114ad791acf21ed2496b7a6df28f0b8
flag81 <flag81@gmail.com>
2024-09-23 11:55:43 +0200

    railway  commit

--- connection2.js
+++ connection2.js
@@ -4,16 +4,17 @@ import mysql from 'mysql2';
 
 const db = mysql.createPool({
   connectionLimit:4,
-  host: "postgresql://admin:3gqLy9yx5alzqZni5ipJyw3EpelrI5Cr@dpg-crm28htumphs73eg4sr0-a.frankfurt-postgres.render.com/zbritje",
+  host: "mysql.railway.internal",
   user: "root",
-  password: "prishtina81",
-  database:"zbritje",
+  port: 3306,
+  password: "sVclMskbFgSLmFwJcrIxhUELEHhSKMQo",
+  database:"railway",
 });
 
 db.getConnection((err,connection)=> {
   if(err)
   throw err;
-  //console.log('Database connected successfully');
+  console.log('Database connected successfully');
   connection.release();
 });
 
