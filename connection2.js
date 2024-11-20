673c9e7b2286fbb436ae161ca89e0ef93adb49dd
Flamur <flag81@gmail.com>
2024-11-20 11:18:27 +0100

    Update connection2.js
    HETZNER

--- connection2.js
+++ connection2.js
@@ -2,7 +2,7 @@
 import mysql from 'mysql2';
 
 
-
+/*
 
 const db = mysql.createPool({
   connectionLimit:4,
@@ -12,6 +12,18 @@ const db = mysql.createPool({
   password: "WQAbQaQZdMpXHhSuqQZRPoIIcufLPycF",
   database:"railway",
 });
+
+*/
+
+const db = mysql.createPool({
+  connectionLimit:4,
+  host: "128.140.43.244",
+  port: 5432,
+  user: "mysql",
+  password: "qbWdiCuFkMWED1TiFp9oY8mHliu2AeHV0uYkgSKXNxR2T7Hx5y8OH6X5jFjdKQi7",
+  database:"default",
+});
+
 
 db.getConnection((err,connection)=> {
   if(err)
