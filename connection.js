89c4185b27915ead496d7a8316cbeda1a98fb2ec
Flamur S <flag8@ptk3tlpt-03.ptk.local>
2025-02-24 22:40:22 +0100

    24022025 2

--- connection.js
+++ connection.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const db = mysql.createPool({
   connectionLimit:10,
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database:process.env.DB_NAME,
